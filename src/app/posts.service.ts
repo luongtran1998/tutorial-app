04375685a0bb05b16b749b0cf276e52e888983b4
luongtx <tranxuanluong1998@gmail.com>
2021-10-07 21:32:48 +0700

    add bootstrap and fix html

---
+++ src/app/posts.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+
+import { Post } from './post.model';
+
+@Injectable({ providedIn: 'root' })
+export class PostsService {
+  API_END_POINT = "https://angular-complete-guide-9aa34-default-rtdb.asia-southeast1.firebasedatabase.app/post.json"
+  error = new Subject<string>();
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(postData: Post) {
+    this.http
+      .post<{ name: string }>(
+        this.API_END_POINT,
+        postData
+      )
+      .subscribe(
+        responseData => {
+          console.log(responseData);
+        },
+        error => {
+          this.error.next(error.message);
+        }
+      );
+  }
+
+  fetchPosts() {
+    return this.http
+      .get<{ [key: string]: Post }>(
+       this.API_END_POINT
+      )
+      .pipe(
+        map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        }),
+        catchError(errorRes => {
+          // Send to analytics server
+          return throwError(errorRes);
+        })
+      );
+  }
+
+  deletePosts() {
+    return this.http.delete(
+      this.API_END_POINT
+    );
+  }
+}
